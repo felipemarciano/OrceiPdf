fcdd288f1950aa456dd9037a22e838ab5d64cc86
Felipe Marciano <felipebsq@gmail.com>
2021-04-24 15:49:08 -0300

    Diagrama de Classes

--- OrceiPdf.Domain/Models/User.cs
+++ OrceiPdf.Domain/Models/User.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 
 namespace OrceiPdf.Domain.Models
 {
     public class User : IdentityUser<Guid>
     {
-
+        public ICollection<Empresa> Empresas { get; set; }
     }
 }
